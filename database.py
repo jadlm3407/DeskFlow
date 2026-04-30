1dc42221b9de0178cbdcb28938603b83f5d668ee
TobiasDold <tobiasdold959@gmail.com>
2026-04-30 16:00:45 +0200

    Prueba Cambios

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URl = "sqlite:///./espacios.db"
+DATABASE_URL = "sqlite:///./espacios.db"
 
 engine = create_engine(
     DATABASE_URL,
